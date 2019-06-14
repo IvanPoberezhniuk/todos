a6e3b5d7664a19a455927f726be9923202a6e0c4
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-06-14 21:48:37 +0300

    Updates

---
+++ precache-manifest.3d2e9b7c0c1fbacc8b8a410d3b16ff70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a844ecd22deecf715e545b2e555591a",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "0cf9f45c52b67883545d",
+    "url": "/todos/static/css/main.cd3fd067.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/todos/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "0cf9f45c52b67883545d",
+    "url": "/todos/static/js/main.ab26bf52.chunk.js"
+  },
+  {
+    "revision": "89246cf4ecee7b6195cb",
+    "url": "/todos/static/js/runtime~main.2b8fbda7.js"
+  }
+]);
